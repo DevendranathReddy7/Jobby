9b24c4868fdbf4ee5fd538ef1e76c3629f2ec305
Devendra <devendranathreddy54@gmail.com>
2024-07-28 08:30:52 +0530

    jobs

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -1,151 +1,123 @@
-import './index.css'
-import {useState, useEffect} from 'react'
-import {useHistory} from 'react-router-dom'
-import NavBar from '../NavBar/index'
-import PrintJob from '../PrintJobs'
-import LoaderComp from '../Loader/index'
+import "./index.css";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import NavBar from "../NavBar/index";
+import PrintJob from "../PrintJobs";
+import LoaderComp from "../Loader/index";
+
+const Cookies = require("js-cookie");
 
 const employmentTypesList = [
   {
-    label: 'Full Time',
-    employmentTypeId: 'FULLTIME',
+    label: "Full Time",
+    employmentTypeId: "FULLTIME",
   },
   {
-    label: 'Part Time',
-    employmentTypeId: 'PARTTIME',
+    label: "Part Time",
+    employmentTypeId: "PARTTIME",
   },
   {
-    label: 'Freelance',
-    employmentTypeId: 'FREELANCE',
+    label: "Freelance",
+    employmentTypeId: "FREELANCE",
   },
   {
-    label: 'Internship',
-    employmentTypeId: 'INTERNSHIP',
+    label: "Internship",
+    employmentTypeId: "INTERNSHIP",
   },
-]
+];
 
 const salaryRangesList = [
   {
-    salaryRangeId: '1000000',
-    label: '10 LPA and above',
+    salaryRangeId: "1000000",
+    label: "10 LPA and above",
   },
   {
-    salaryRangeId: '2000000',
-    label: '20 LPA and above',
+    salaryRangeId: "2000000",
+    label: "20 LPA and above",
   },
   {
-    salaryRangeId: '3000000',
-    label: '30 LPA and above',
+    salaryRangeId: "3000000",
+    label: "30 LPA and above",
   },
   {
-    salaryRangeId: '4000000',
-    label: '40 LPA and above',
+    salaryRangeId: "4000000",
+    label: "40 LPA and above",
   },
-]
+];
 
 const Jobs = () => {
-  const history = useHistory()
-  const [isLoading, setIsLoading] = useState(false)
+  const history = useHistory();
+  const [isLoading, setIsLoading] = useState(false);
   const [queryParams, setQueryParam] = useState({
     empType: [],
-    salary: '',
-    search: '',
-  })
-  const [profileDetails, setProfileDetails] = useState({
-    profile_details: {
-      name: 'Rahul Attuluri',
-      profile_image_url:
-        'https://assets.ccbp.in/frontend/react-js/male-avatar-img.png',
-      short_bio: 'Lead Software Developer and AI-ML expert',
-    },
-  })
-  const [jobs, setJobs] = useState([
-    {
-      company_logo_url:
-        'https://assets.ccbp.in/frontend/react-js/jobby-app/facebook-img.png',
-      employment_type: 'Full Time',
-      id: 'd6019453-f864-4a2f-8230-6a9642a59466',
-      job_description:
-        'We’re in search of a Back-End Software Engineer that specializes in server-side components. In this role, you’ll primarily work in NodeJs, SQL Lite, Python, AWS and GO and will bring a depth of knowledge on basic algorithms and data structures. As a Back-End Engineer, you might be architecting new features for our customers.',
-      location: 'Bangalore',
-      package_per_annum: '21 LPA',
-      rating: 4,
-      title: 'Backend Engineer',
-    },
-    {
-      company_logo_url:
-        'https://assets.ccbp.in/frontend/react-js/jobby-app/facebook-img.png',
-      employment_type: 'Full Time',
-      id: 'd6019453-f864-4a2f-8230-6a9642a59466',
-      job_description:
-        'We’re in search of a Back-End Software Engineer that specializes in server-side components. In this role, you’ll primarily work in NodeJs, SQL Lite, Python, AWS and GO and will bring a depth of knowledge on basic algorithms and data structures. As a Back-End Engineer, you might be architecting new features for our customers.',
-      location: 'Bangalore',
-      package_per_annum: '21 LPA',
-      rating: 4,
-      title: 'Backend Engineer',
-    },
-  ])
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-  }
+    salary: "",
+    search: "",
+  });
+  const [profileDetails, setProfileDetails] = useState();
+  const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
 
     const getJobs = async () => {
       const response = await fetch(
         `https://apis.ccbp.in/jobs?employment_type=${queryParams.empType.join(
-          ',',
+          ","
         )}&minimum_package=${queryParams.salary}&search=${queryParams.search}`,
-        requestOptions,
-      )
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            Authorization: `Bearer ${Cookies.get("jwt_token")}`,
+          },
+        }
+      );
 
-      const data = await response.json()
-      setJobs(data)
-    }
-    getJobs()
-    setIsLoading(false)
-  }, [queryParams, requestOptions])
+      const data = await response.json();
+      console.log(data);
+      setJobs(data);
+    };
+    getJobs();
+    setIsLoading(false);
+  }, [queryParams]);
 
   useEffect(() => {
     const getProfile = async () => {
-      const response = await fetch('https://apis.ccbp.in/profile', {
-        method: 'GET',
+      const response = await fetch("https://apis.ccbp.in/profile", {
+        method: "GET",
         headers: {
-          authorization:
-            '"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJhaHVsIiwicm9sZSI6IlBSSU1FX1VTRVIiLCJpYXQiOjE2MTk2Mjg2MTN9. nZDlFsnSWArLKKeF0QbmdVfLgzUbx1BGJsqa2kc_21Y',
+          Authorization: `Bearer ${Cookies.get("jwt_token")}`,
         },
-      })
-      const data = await response.json()
-      setProfileDetails(data)
-    }
-    // getProfile()
-    setIsLoading(false)
-  }, [])
+      });
+      const data = await response.json();
+      setProfileDetails(data);
+    };
+    getProfile();
+    setIsLoading(false);
+  }, []);
 
-  const jobHandler = id => {
-    history.push(`/jobs/${id}`)
-  }
+  const jobHandler = (id) => {
+    history.push(`/jobs/${id}`);
+  };
 
   const jobSearchHandler = (value, field) => {
     switch (field) {
-      case 'empType':
-        setQueryParam(prev => ({...prev, empType: [...prev.empType, value]}))
-        break
-      case 'salary':
-        setQueryParam(prev => ({...prev, salary: value}))
-        break
-      case 'search':
-        setQueryParam(prev => ({...prev, search: value}))
-        break
+      case "empType":
+        setQueryParam((prev) => ({
+          ...prev,
+          empType: [...prev.empType, value],
+        }));
+        break;
+      case "salary":
+        setQueryParam((prev) => ({ ...prev, salary: value }));
+        break;
+      case "search":
+        setQueryParam((prev) => ({ ...prev, search: value }));
+        break;
       default:
-        break
+        break;
     }
-  }
+  };
 
   return (
     <div>
@@ -154,24 +126,24 @@ const Jobs = () => {
         <div className="sideBar__div">
           <div className="profile__div">
             <img
-              src={profileDetails.profile_details.profile_image_url}
+              src={profileDetails?.profile_details?.profile_image_url}
               alt=""
               className="profile__icon"
             />
-            <p className="name">{profileDetails.profile_details.name}</p>
-            <p className="role">{profileDetails.profile_details.short_bio}</p>
+            <p className="name">{profileDetails?.profile_details.name}</p>
+            <p className="role">{profileDetails?.profile_details.short_bio}</p>
           </div>
 
           <div className="job__type__div">
             <p>Type of Employment</p>
-            {employmentTypesList.map(job => (
+            {employmentTypesList.map((job) => (
               <li key={job.employmentTypeId} className="job__li">
                 <input
                   type="checkBox"
                   className="checkBox"
                   id="jobType"
                   onChange={() =>
-                    jobSearchHandler(job.employmentTypeId, 'empType')
+                    jobSearchHandler(job.employmentTypeId, "empType")
                   }
                 />
                 <label htmlFor="jobType" className="checkBox__label">
@@ -183,7 +155,7 @@ const Jobs = () => {
 
           <div className="salary__div">
             <p>Salary Range</p>
-            {salaryRangesList.map(salary => (
+            {salaryRangesList.map((salary) => (
               <li key={salary.salaryRangeId} className="salary__li">
                 <input
                   type="radio"
@@ -191,7 +163,7 @@ const Jobs = () => {
                   id="salary"
                   name="salary"
                   onChange={() =>
-                    jobSearchHandler(salary.salaryRangeId, 'salary')
+                    jobSearchHandler(salary.salaryRangeId, "salary")
                   }
                 />
                 <label htmlFor="salary" className="radio__label">
@@ -208,13 +180,13 @@ const Jobs = () => {
               type="search"
               className="input__search"
               placeholder="Find Jobs"
-              onChange={e => jobSearchHandler(e.target.value, 'search')}
+              onChange={(e) => jobSearchHandler(e.target.value, "search")}
             />
           </div>
 
           {!isLoading && (
             <div className="jobs__list__div">
-              {jobs.map(job => (
+              {jobs.map((job) => (
                 <li
                   className="job__li__item"
                   key={job.id}
@@ -230,7 +202,7 @@ const Jobs = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Jobs
+export default Jobs;
